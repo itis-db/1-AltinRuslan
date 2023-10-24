2d16d70328cc720454654e13b5c885213357c6b3
AltinRuslan <altinbaev.ruslan.2003@gmail.com>
2023-10-24 12:33:56 +0300

    added test and changed readme

--- activity.sql
+++ activity.sql
@@ -1,15 +1,37 @@
--- рекурсия 
-WITH RECURSIVE r AS (
-    SELECT 
-        1 AS i, 
-        1 AS factorial
-    
-    UNION 
+CREATE OR REPLACE FUNCTION GetHierarchy(pn_activityid INT DEFAULT NULL) RETURNS TABLE (
+    ActivityId INT,
+    ActivityName VARCHAR(255),
+    ActivityType VARCHAR(255)
+) AS $$
+BEGIN
+    RETURN QUERY
+    WITH RECURSIVE ActivityHierarchy AS (
+        SELECT
+            A.ActivityId,
+            A.Name AS ActivityName,
+            AT.Name AS ActivityType
+        FROM
+            Activity A
+        JOIN ActivityType AT ON A.ActivityTypeId = AT.ActivityTypeId
+        WHERE
+            A.ActivityId = COALESCE(pn_activityid, A.ActivityId)
 
-    SELECT 
-        i+1 AS i, 
-        factorial * (i+1) as factorial 
-    FROM r
-    WHERE i < 10
-)
-SELECT * FROM r;
+        UNION ALL
+
+        SELECT
+            A.ActivityId,
+            A.Name AS ActivityName,
+            AT.Name AS ActivityType
+        FROM
+            Activity A
+        JOIN ActivityHierarchy AH ON A.ParentId = AH.ActivityId
+        JOIN ActivityType AT ON A.ActivityTypeId = AT.ActivityTypeId
+    )
+    SELECT
+        ActivityId,
+        ActivityName,
+        ActivityType
+    FROM
+        ActivityHierarchy;
+END;
+$$ LANGUAGE plpgsql;
--- tables.sql
+++ tables.sql
@@ -1,11 +1,38 @@
-
-create table if not exists activitytype
-(
-    activitytypeid integer not null
-        constraint pk_activitytype
-            primary key,
-    name           text    not null,
-    sysname        text    not null
+CREATE TABLE ActivityType (
+    ActivityTypeId SERIAL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL
 );
 
--- 
+CREATE TABLE Activity (
+    ActivityId SERIAL PRIMARY KEY,
+    ActivityTypeId INT REFERENCES ActivityType(ActivityTypeId),
+    Name VARCHAR(255) NOT NULL,
+    ParentId INT REFERENCES Activity(ActivityId),
+    AreaId INT
+);
+
+CREATE TABLE Program (
+    ActivityId INT PRIMARY KEY REFERENCES Activity(ActivityId),
+    ProgramDetails VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE SubProgram (
+    ActivityId INT PRIMARY KEY REFERENCES Activity(ActivityId),
+    SubProgramDetails VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Project (
+    ActivityId INT PRIMARY KEY REFERENCES Activity(ActivityId),
+    ProjectDetails VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Contract (
+    ActivityId INT PRIMARY KEY REFERENCES Activity(ActivityId),
+    ContractDetails VARCHAR(255) NOT NULL,
+    AreaId INT
+);
+
+CREATE TABLE Point (
+    ActivityId INT PRIMARY KEY REFERENCES Activity(ActivityId),
+    PointDetails VARCHAR(255) NOT NULL
+);
